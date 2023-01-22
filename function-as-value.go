365bf063f64e8052a6684ed0bb299bb4d4763f32
ramarff <rama21010ti@student.nurulfikri.ac.id>
2023-01-22 09:10:37 +0700

    function value

---
+++ function-as-value.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func getGoodBye(name string) string {
+	return "Good Bye " + name
+}
+
+func main() {
+	sayGoodBye := getGoodBye
+
+	result := sayGoodBye("Joko Widodo")
+
+	fmt.Println(result)
+}
