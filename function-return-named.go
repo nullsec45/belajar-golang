f3c6aa43b71773a5b251e0d12d865f7b0dba0e36
ramarff <rama21010ti@student.nurulfikri.ac.id>
2023-01-22 07:43:11 +0700

    function-return-named

--- function-return-named.go
+++ function-return-named.go
@@ -1,1 +1,20 @@
 package main
+
+import "fmt"
+
+// Tidak perlu memanggil nama varibalenya lagi ketika return, karena
+// sudah deklarasi pada saat inisiasi functionnya
+func getFullName2() (firstName, middleName, lastName string) {
+	firstName = "Rama"
+	middleName = "Fajar"
+	lastName = "Fadhillah"
+
+	return
+}
+
+func main() {
+	firstName, middleName, lastName := getFullName2()
+	fmt.Println(firstName)
+	fmt.Println(middleName)
+	fmt.Println(lastName)
+}
